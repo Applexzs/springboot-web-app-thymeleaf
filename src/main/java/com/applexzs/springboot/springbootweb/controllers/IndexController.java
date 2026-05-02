2cf86a0419fc6b9f7a366e6e60be7d9abab10b14
Applexzs <reverseapplemw2@gmail.com>
2026-05-02 13:17:49 -0600

    Project setup

---
+++ src/main/java/com/applexzs/springboot/springbootweb/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.applexzs.springboot.springbootweb.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = {"/index", "/", "/home"})
+    public String index() {
+        return "index";
+    }
+
+}
